ec4fc3e4149fba1fb48fb8e7b6a4be6bfcb93fa3
Muhamad Rizky Rahmansyah <muhamad.rizky1996@gmail.com>
2021-02-18 16:40:31 +0700

    update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -27,43 +27,6 @@ class BookController extends Controller
         return BookResource::collection($books);
     }
 
-    public function index_test()
-    {
-        $books = Book::paginate(10);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-        return BookResource::collection($books);
-    }
-
-
     /**
      * Store a newly created resource in storage.
      *
